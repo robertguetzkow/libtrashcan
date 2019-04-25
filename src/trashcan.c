robertguetzkow/libtrashcan

commit 6544f3cbe4cd780a3cf2e168e0ca08785afdc261

Use MultiByteToWideChar instead of mbstowcs_s for conversion
This works properly when path is an actual multi byte sequence. However in case of a CLI application, this requires that the console or powershell actually passes all characters. See issue #2 for cases where this doesn't happen.

src/trashcan.c
@@ -41,6 +41,7 @@
 #include "trashcan.h"
 
 #ifdef WIN32
+#include <Windows.h>
 #include <objbase.h>
 #include <shellapi.h>
 #include <shobjidl.h>
@@ -187,20 +188,20 @@ int soft_delete_com(const char *path, bool init_com)
 {
 	int status = LIBTRASHCAN_SUCCESS;
 	wchar_t *wcs = NULL;
-	size_t mbslen = 0;
-	size_t temp = 0;
 
-	/* Determine the length (mbslen includes the zero terminator) */
-	if (mbstowcs_s(&mbslen, NULL, 0, path, 0) != 0) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARLEN, error_0) }
+	size_t mbslen = MultiByteToWideChar(CP_OEMCP, 0, path, -1, NULL, 0);
+	if (mbslen == 0) { goto error_0; }
 
-	wcs = calloc(mbslen, sizeof(wchar_t));
-	if (wcs == NULL) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARALLOC, error_0) }
+	wcs = calloc(mbslen, sizeof(wchar_t)); /* Length includes zero termination */
 
-	/* Convert to wchar_t */
-	if (mbstowcs_s(&temp, wcs, mbslen, path, mbslen - 1) != 0) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARCONV, error_0) }
+	if (wcs == NULL) { goto error_0; }
 
-	return soft_delete_internal(wcs, init_com);
+	if (MultiByteToWideChar(CP_OEMCP, 0, path, -1, wcs, mbslen) == 0) { goto error_1; }
 
+	status = soft_delete_internal(wcs, init_com);
+
+error_1:
+	free(wcs);
 error_0:
 	return status;
 }

commit 8f16acd7056acc18e083c71f1f38d0f9f8e5e8ca

Corrected error handling mistakes in soft_delete_com introduced in 6544f3c

src/trashcan.c
@@ -190,13 +190,13 @@ int soft_delete_com(const char *path, bool init_com)
 	wchar_t *wcs = NULL;
 
 	size_t mbslen = MultiByteToWideChar(CP_OEMCP, 0, path, -1, NULL, 0);
-	if (mbslen == 0) { goto error_0; }
+	if (mbslen == 0) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARLEN, error_0) }
 
 	wcs = calloc(mbslen, sizeof(wchar_t)); /* Length includes zero termination */
 
-	if (wcs == NULL) { goto error_0; }
+	if (wcs == NULL) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARALLOC, error_0) }
 
-	if (MultiByteToWideChar(CP_OEMCP, 0, path, -1, wcs, mbslen) == 0) { goto error_1; }
+	if (MultiByteToWideChar(CP_OEMCP, 0, path, -1, wcs, mbslen) == 0) { HANDLE_ERROR(status, LIBTRASHCAN_WCHARCONV, error_1) }
 
 	status = soft_delete_internal(wcs, init_com);
 